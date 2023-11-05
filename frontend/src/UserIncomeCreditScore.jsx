482cd589ea5790277999da8f5356dcf04abe9529
Amber Hasan <amber.hasan@gmail.com>
2023-11-04 22:52:47 -0500

    Fixed slider UI

--- frontend/src/UserIncomeCreditScore.jsx
+++ frontend/src/UserIncomeCreditScore.jsx
@@ -4,31 +4,34 @@ import React from "react";
 const UserIncomeCreditScore = ({ formData, handleChange }) => {
   return (
     <div>
-      <label>
-        Credit Score
-        <input
-          type="range"
-          name="creditScore"
-          min="300"
-          max="850"
-          value={formData.creditScore}
-          onChange={handleChange}
-        />
-        {formData.creditScore}
-      </label>
-
-      <label>
-        Monthly Income (Gross)
-        <input
-          type="range"
-          name="monthlyIncome"
-          min="0"
-          max="100000"
-          value={formData.monthlyIncome}
-          onChange={handleChange}
-        />
-        {formData.monthlyIncome}
-      </label>
+      <div>
+        <label>
+          Credit Score
+          <input
+            type="range"
+            name="creditScore"
+            min="300"
+            max="850"
+            value={formData.creditScore}
+            onChange={handleChange}
+          />
+          {formData.creditScore}
+        </label>
+      </div>
+      <div>
+        <label>
+          Monthly Income (Gross)
+          <input
+            type="range"
+            name="monthlyIncome"
+            min="0"
+            max="100000"
+            value={formData.monthlyIncome}
+            onChange={handleChange}
+          />
+          {formData.monthlyIncome}
+        </label>
+      </div>
     </div>
   );
 };
